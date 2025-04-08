620cfe4d07e73cbf793cf5846af66875f5e2136f
NickK89 <teodoric89@gmail.com>
2025-04-08 23:08:12 +0400

    Created

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://1196b3cb-8d2f-416b-82cb-fc99e3224ddc.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://12103360-d1e0-469d-a829-889f98715266.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_PATH = "/api/v1/orders/track?t="
